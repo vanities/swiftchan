a38e512c4b7641f0ec3ac8ce3e881db481333119
Adamant Mischke <mischkeaa@gmail.com>
2020-10-30 20:55:52 -0500

    initial commit

---
+++ swiftchan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftchan
+//
+//  Created by vanities on 10/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftchan/swiftchanApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftchanApp.swift
+//  swiftchan
+//
+//  Created by vanities on 10/30/20.
+//
+
+import SwiftUI
+
+@main
+struct swiftchanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
