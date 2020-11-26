bc7d75e5463d302ec383894c1a2c7f876cc9f1c1
Adamant Mischke <mischkeaa@gmail.com>
2020-11-26 01:44:55 -0600

    make gallery dragging better

---
+++ swiftchan/Views/Blur.swift
@@ -0,0 +1,18 @@
+//
+//  Blur.swift
+//  swiftchan
+//
+//  Created by vanities on 11/26/20.
+//
+
+import SwiftUI
+
+struct Blur: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemMaterial
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
--- swiftchan/Views/Thread/RepliesView.swift
+++ swiftchan/Views/Thread/RepliesView.swift
@@ -17,24 +17,33 @@ struct RepliesView: View {
     @State var presentingSheet: PresentingSheet = .replies
 
     var body: some View {
-        return ForEach(self.replies, id: \.self) { index in
-            PostView(boardName: self.viewModel.boardName,
-                     post: self.viewModel.posts[index],
-                     index: index,
-                     comment: self.viewModel.comments[index],
-                     replies: self.viewModel.replies[index] ?? nil,
-                     isPresenting: self.$isPresenting,
-                     presentingSheet: self.$presentingSheet,
-                     galleryIndex: self.$postIndex,
-                     commentRepliesIndex: self.$commentRepliesIndex
-            )
-        }
+        return
+            ZStack {
+                Blur(style: .regular)
+                ScrollView {
+                    ForEach(self.replies, id: \.self) { index in
+                        PostView(boardName: self.viewModel.boardName,
+                                 post: self.viewModel.posts[index],
+                                 index: index,
+                                 comment: self.viewModel.comments[index],
+                                 replies: self.viewModel.replies[index] ?? nil,
+                                 isPresenting: self.$isPresenting,
+                                 presentingSheet: self.$presentingSheet,
+                                 galleryIndex: self.$postIndex,
+                                 commentRepliesIndex: self.$commentRepliesIndex
+                        )
+                    }
+                }
+                //.background(Blur(style: .regular).)
+            }.ignoresSafeArea(edges: .bottom)
     }
 }
 
 struct RepliesView_Previews: PreviewProvider {
+
+    // TODO: doesn't work
     static var previews: some View {
-        let viewModel = ThreadView.ViewModel(boardName: "fit", id: 5551578)
-        RepliesView(replies: [0], viewModel: viewModel)
+        let viewModel = ThreadView.ViewModel(boardName: "g", id: 76759434)
+        RepliesView(replies: [0, 1], viewModel: viewModel)
     }
 }
