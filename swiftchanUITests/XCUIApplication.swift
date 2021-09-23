fc6dcb72c55dc17809636db319ab2b0567653740
Adamant Mischke <mischkeaa@gmail.com>
2021-09-23 00:10:11 -0500

    add a ton of error messages

--- swiftchanUITests/XCUIApplication.swift
+++ swiftchanUITests/XCUIApplication.swift
@@ -1,47 +1,50 @@
 import XCTest
 
 extension XCUIApplication {
-    func tapElement(_ element: XCUIElement, _ timeout: TimeInterval = 1) {
-        XCTAssertTrue(element.waitForExistence(timeout: timeout), "Could not tap element! \(element.debugDescription)")
+    func tapElement(_ element: XCUIElement, _ timeout: TimeInterval = 1, errorMessage: String? = nil) {
+        assertExistence(element, timeout, errorMessage: errorMessage ?? "Could not tap element! \(element.debugDescription)")
         element.tap()
     }
-    func longPressElement(_ element: XCUIElement, _ duration: TimeInterval = 0.5, _ timeout: TimeInterval = 1) {
-        XCTAssertTrue(element.waitForExistence(timeout: timeout), "Could not long press element! \(element.debugDescription)")
+    func longPressElement(_ element: XCUIElement,
+                          _ duration: TimeInterval = 0.5,
+                          _ timeout: TimeInterval = 1,
+                          errorMessage: String? = nil) {
+        assertExistence(element, timeout, errorMessage: errorMessage ?? "Could not long press element! \(element.debugDescription)")
         element.press(forDuration: duration)
     }
-    func assertExistence(_ element: XCUIElement, _ timeout: TimeInterval = 1) {
-        XCTAssert(element.waitForExistence(timeout: timeout), "Could not assert element! \(element.debugDescription)")
+    func assertExistence(_ element: XCUIElement, _ timeout: TimeInterval = 1, errorMessage: String? = nil) {
+        XCTAssert(element.waitForExistence(timeout: timeout), errorMessage ?? "Could not assert element! \(element.debugDescription)")
     }
 
     func goToBoard(_ name: String) {
-        tapElement(boardButton(name))
+        tapElement(boardButton(name), errorMessage: "Could not go to /\(name)/ board!")
     }
     func goToOPThread(_ index: Int) {
-        tapElement(opButton(index))
+        tapElement(opButton(index), errorMessage: "Could not go to \(index) op thread!")
     }
     func tapThumbnailMedia(_ index: Int) {
-        tapElement(thumbnailMediaImage(index))
+        tapElement(thumbnailMediaImage(index), errorMessage: "Could not tap \(index) thumbnail media!")
     }
     func longPressGalleryMedia(_ index: Int) {
-        longPressElement(galleryMediaImage(index))
+        longPressElement(galleryMediaImage(index), errorMessage: "Could not long press \(index) gallery media!")
     }
     func tapSaveToPhotosButton() {
-        tapElement(saveToPhotosButton)
+        tapElement(saveToPhotosButton, errorMessage: "Could not tap to save photos buttons!")
     }
     func tapCopyToPasteboardButton() {
-        tapElement(copyToPasteboardButton)
+        tapElement(copyToPasteboardButton, errorMessage: "Could not press copy to pasteboard button!")
     }
 
     func assertBoardButton(_ name: String) {
-        assertExistence(boardButton(name))
+        assertExistence(boardButton(name), errorMessage: "Could not assert board button \(name)!")
     }
     func assertOpButton(_ index: Int) {
-        assertExistence(opButton(index))
+        assertExistence(opButton(index), errorMessage: "Could not assert op button \(index)!")
     }
     func assertPost(_ index: Int) {
-        assertExistence(postText(index))
+        assertExistence(postText(index), errorMessage: "Could not assert post \(index)!")
     }
     func assertSuccessToastImage() {
-        assertExistence(successToastImage)
+        assertExistence(successToastImage, errorMessage: "Could not assert success toast image!")
     }
 }
